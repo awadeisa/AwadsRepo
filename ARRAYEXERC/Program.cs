3614d3f20ee63034d4bf871122b6994921e3b721
Awad Eisa <awad.eisa@knowe.se>
2019-03-18 13:01:15 +0100

    Add project files.

---
+++ ARRAYEXERC/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARRAYEXERC
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("How many studens in your class");
+			var studentcount = int.Parse(Console.ReadLine());
+
+			var studentsname = new string[studentcount];
+			var studentgrades = new int[studentcount];
+
+			for (int i = 0; i < studentcount; i++)
+			{
+				Console.Write("studen name: ");
+				studentsname[i] = Console.ReadLine();
+				Console.Write("studen grade: ");
+				studentgrades[i] = int.Parse(Console.ReadLine());
+			}
+			for (int i = 0; i < studentcount; i++)
+			{
+				Console.WriteLine("student name: {0}, grade: {1} ", studentsname[i], studentgrades[i]);
+			}
+		}
+	}
+}
---
+++ ARRAYEXERC/arraysStudentcount.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARRAYEXERC
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("How many studens in your class");
+
+			var students = new List<Student>();
+			
+
+			
+			var adding = true;
+			while (adding)
+			{
+
+				var newStudent = new Student();
+				//Console.Write("studen name: ");
+				//newStudent.Name = (Console.ReadLine());
+				newStudent.Name = util.Console.Ask("student name: ");
+
+				//Console.Write("studen grade: ");
+				//newStudent.Grade = int.Parse(Console.ReadLine());
+				newStudent.Grade = int.Parse(util.Console.Ask("student Grade: "));
+
+				//Console.WriteLine("Student Birthday");
+				//newStudent.BIrthday = Console.ReadLine();
+				newStudent.BIrthday = util.Console.Ask("Student Birthday: ");
+
+				//Console.WriteLine("Student Address");
+				//newStudent.Address = Console.ReadLine();
+				newStudent.Address = util.Console.Ask("student Address: ");
+
+				//Console.WriteLine("Student Phone");
+				//newStudent.SetPhone(int.Parse(Console.ReadLine()));
+				newStudent.Phone = int.Parse(util.Console.Ask("student Phone: "));
+
+				students.Add(newStudent);
+				Student.count++;
+				Console.WriteLine("studen count is: {0}", Student.count);
+
+
+				Console.WriteLine("add another y/n");
+				if (Console.ReadLine() != "y")
+				{
+					adding = false;
+				}
+			}
+
+			foreach (var Student in students)
+			{
+
+		
+				Console.WriteLine("student name: {0}, grade: {1} ", Student.Name, Student.Grade);
+			}
+		}
+	}
+
+	class Student
+	{
+		public int Phone
+		{
+			set { phone = value; }
+		}
+		public void SetPhone(int number)
+		{
+			phone = number;
+		}
+
+		static public int count = 0;
+		public string Name;
+		public int Grade;
+		public string BIrthday;
+		public string Address;
+		private int phone;
+
+
+	}
+}
+
